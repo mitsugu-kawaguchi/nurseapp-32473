ec05cf0cbb20e0aed93b4eeb13059981100851b3
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-30 10:59:28 +0900

    編集・削除機能実装

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -21,6 +21,32 @@ class TechniquesController < ApplicationController
     @technique = Technique.find(params[:id])
   end
 
+  def edit
+    @technique = Technique.find(params[:id])
+    unless current_user.nursing_history >= 4
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @technique = Technique.find(params[:id])
+    if @technique.update(techniques_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @technique = Technique.find(params[:id])
+    if current_user.nursing_history >= 4
+      @technique.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
 
   def techniques_params
